aba7fa0155c7e350a2a9fac8a642dc460a1e7c31
labflores <eve.99.ef@gmail.com>
2026-07-24 15:36:55 -0300

    Create app.py
    Código de la aplicación

---
+++ app.py
@@ -0,0 +1,117 @@
+import streamlit as st
+
+# ----------------------------------------------------
+# Configuración de la página
+# ----------------------------------------------------
+st.set_page_config(
+    page_title="Viajes Inteligentes",
+    page_icon="✨✈️✨",
+    layout="centered"
+)
+
+# ----------------------------------------------------
+# Título y descripción
+# ----------------------------------------------------
+st.title("Viajes Inteligentes✈️")
+
+st.write(
+    """
+    Encontrá oportunidades de viaje según tu presupuesto 
+    y tiempo disponible.
+    """
+)
+#-----------------------------------------------------
+# Búsqueda
+#-----------------------------------------------------
+with st.form("busqueda"):
+
+    origen = st.text_input("Ciudad de origen")
+
+    destino = st.text_input("Destino")
+
+    fecha_inicio = st.date_input("Fecha de salida")
+
+    fecha_fin = st.date_input("Fecha de regreso")
+
+    presupuesto = st.number_input(
+        "Presupuesto máximo",
+        min_value=0
+    )
+
+    buscar = st.form_submit_button("Buscar oportunidad")
+
+#------------------------------------------------------
+if buscar:
+
+    if origen == "" or destino == "":
+        st.error("Completá origen y destino.")
+
+    elif fecha_fin < fecha_inicio:
+        st.error("La fecha de regreso no puede ser anterior.")
+
+    else:
+
+#-----------------------------------------------------
+# Contenedor de resultados
+#-----------------------------------------------------
+with st.container():
+
+    st.success("Búsqueda realizada")
+
+    st.subheader("Resumen")
+
+    st.write(f"**Origen:** {origen}")
+
+    st.write(f"**Destino:** {destino}")
+
+    st.write(f"**Presupuesto:** USD {presupuesto}")
+
+#Resultado
+
+post = f"""
+✈️ {destino.upper()}
+
+🔥 Oferta encontrada
+
+📅 {fecha_inicio:%d/%m/%Y}
+
+💰 Desde USD {presupuesto}
+
+🌎  ¡Animate! ✨
+"""
+
+st.info(post)
+
+#--------------------------------------------------
+#Funciones
+# -----------------------------------------------
+def mostrar_post_instagram(destino, fecha, presupuesto):
+
+    return f"""
+✈️ {destino.upper()}
+
+🔥 Oferta encontrada
+
+📅 {fecha}
+
+💰 Desde USD {presupuesto}
+
+Guardá este post.
+"""
+
+#Interfaz
+
+col1, col2 = st.columns(2)
+
+with col1:
+    origen = st.text_input("Origen")
+
+with col2:
+    destino = st.text_input("Destino")
+
+
+
+
+
+
+
